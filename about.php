31e03dfc50c686b517b7def7328e9a73ecb4c5bc
AnushkaShah10 <anushka.shah.c104@gmail.com>
2024-05-27 17:09:44 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,89 @@
+<?php
+    include "includes/header.php";
+?>
+		<div class="col-md-9 main" >
+		<!--banner-section-->
+            <div class="banner-section">
+            <!--/top-news-->
+            <div class="top-news">
+                <div class="about-content">
+                    <!-- about -->
+                    <h3 class="tittle">About <i class="glyphicon glyphicon-user"></i></h3>
+                    <img src="image_uploads/logo.png" alt="" />
+                    <p>Having hands on experience in creating innovative designs, We do offer design
+                        solutions which harness the new possibilities of web based communication.
+                        <label>We do specialize in all aspects of website designing.</label></p>
+                    <div class="clearfix"></div>
+                    <p class="nam">The  “PLACEMENT AUTOMATION SYSTEM”  is developed to maintain the details of student information and information about the company and their vacancies.
+This project is aimed at developing an web application for the Placement Cell of the college.
+Students logging should be able to upload their personal details and resume.Also companies can list on the placement cell and they can post their vacancies.This allow registered students to directly apply for that vacancies.
+</p>
+
+                </div>
+                <!--div class="top-inner second">
+                    <div class="col-md-6 top-text">
+                        <a href="single.html"><img src="images/pic1.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 25 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="col-md-6 top-text two">
+                        <a href="single.html"><img src="images/pic2.jpg" class="img-responsive" alt=""></a>
+                        <h5 class="top"><a href="single.html">Consetetur sadipscing elit</a></h5>
+                        <p>Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt labore dolore magna aliquyam eratsed diam justo duo dolores rebum.</p>
+                        <p>On Jun 27 <a class="span_link" href="#"><span class="glyphicon glyphicon-comment"></span>0 </a><a class="span_link" href="#"><span class="glyphicon glyphicon-eye-open"></span>56 </a><a class="span_link" href="single.html"><span class="glyphicon glyphicon-circle-arrow-right"></span></a></p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div-->
+            </div>
+            <!--//top-news-->
+        </div>
+    <!--//banner-section-->
+    <div class="banner-right-text">
+        <h3 class="tittle">Our Team <i class="glyphicon glyphicon-star"></i></h3>
+        <!--/general-news-->
+        <div class="general-news">
+            <div class="general-inner">
+                <div class="edit-pics">
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f4.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">Amruta Deokate</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                        <div class="col-md-9 item-details">
+                            <h5 class="inner two"><a href="single.html">Anushka Shah</a></h5>
+                            <div class="td-post-date two"></div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="editor-pics">
+                        <div class="col-md-3 item-pic">
+                            <img src="image_uploads/f1.jpg" class="img-responsive" alt="">
+
+                        </div>
+                       
+                        <div class="clearfix"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--//general-news-->
+        <!--/news-->
+        <!--/news-->
+    </div>
+    </div>
+    <div class="clearfix"> </div>
+<?php
+include "includes/footer.php";
+?>
